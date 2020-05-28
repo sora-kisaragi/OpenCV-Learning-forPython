5e2abda21d4aa946c77338d543fbcd9178d8d52e
sora-kisaragi <soramunehiro.game@gmail.com>
2020-05-28 17:50:02 +0900

    ファイル構成変更に伴うソースコードの変更

--- Source/CannyFilter.py
+++ Source/CannyFilter.py
@@ -6,7 +6,7 @@ img = cv2.imread("Photo/lean.png")
 img2 = cv2.Canny(img, 30, 200)
 
 # ネガポジ反転
-img2 = 256 - img2
+# img2 = 256 - img2
 
 # ファイルに保存
 cv2.imwrite("Photo/lean-canny.png", img2)
--- Source/ConvertGrayPhoto.py
+++ Source/ConvertGrayPhoto.py
@@ -3,4 +3,4 @@ import cv2
 
 img = cv2.imread("Photo/lean.png")
 gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("Photo/lean-gray.jpg", gry)
+cv2.imwrite("Photo/lean-gray2.jpg", gry)
